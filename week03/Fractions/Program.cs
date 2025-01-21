b7b71f3b26b523a82f7ffc32e005425db6108e43
berrypatchwhims <berrypatchgirl@myyahoo.com>
2025-01-20 16:01:15 -0800

    Fractions Functional

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(1,3);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        // f1.SetTop(3);
+        // f1.SetBottom(5);
+        // Console.WriteLine(f1.GetTop());
+        // Console.WriteLine(f1.GetBottom());
+
+
+
+
     }
 }
