5962a5305e58e90ef295bef69570ea91ec4bd1a8
berrypatchwhims <berrypatchgirl@myyahoo.com>
2025-02-03 17:34:02 -0800

    Inheritance Learning Activity: Homework- Done

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Samuel Bennet", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment ma1 = new MathAssignment("7.3", "8-19", "Martha Sanchez", "Fractions");
+        Console.WriteLine(ma1.GetSummary());
+        Console.WriteLine(ma1.GetHomeworkList());
+
+        WritingAssignment wa1 = new WritingAssignment("Mary Waters", "History","What Caused WWI");
+        Console.WriteLine(wa1.GetSummary());
+        Console.WriteLine(wa1.GetWritingInfo());
     }
 }
